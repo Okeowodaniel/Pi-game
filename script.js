eb6f9731b479816e38d39711304aa57044a2efcc
Okeowodaniel <aded3410@gmail.com>
2023-08-12 09:23:49 +0100

    Functionality of the gane

---
+++ script.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// Selectiing elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+const swithPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// Rolling dice funtionallity
+btnRoll.addEventListener('click', function () {
+  // 1. Generating random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3.Check for rolled 1: if true,
+  if (dice !== 1) {
+    // Add the dicce to sthe current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // swith player
+    swithPlayer();
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  // add current score to active player
+  scores[activePlayer] += currentScore;
+  // or
+  // score[1] = scores[1] += currentScore;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  // 2. Check if player score is >= 100
+  // Finish the game
+
+  // Switch to the next Player
+  switchPlayer();
+});
